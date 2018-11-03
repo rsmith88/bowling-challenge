a344e898c16ec35a8573a00af6dc91662227ec94
Raylene <smith.raylene@gmail.com>
2018-11-03 14:37:32 +0000

    Added tests and functions for secondRoll

--- spec/RoundSpec.js
+++ spec/RoundSpec.js
@@ -6,39 +6,45 @@ describe("Round", function() {
   });
 
   describe("score", function() {
-    it("should return the total score of the round", function() {
+    it("should return the total score of the round (roundScore)", function() {
       expect(round1.score()).toEqual(0);
     });
   });
 
   describe("firstRoll", function() {
-    it("if strike, it should add 10 points to roundscore & record strike", function() {
+    it("if strike, it should add 10 points to roundScore & record strike", function() {
       round1.firstRoll('strike');
       expect(round1.score()).toEqual(10);
-      expect(round1._strike).toEqual(true)
+      expect(round1.strike).toEqual(true)
     });
 
-    it("if not strike, it should add roll points to roundscore", function() {
+    it("if not strike, it should add rollPoints to roundScore", function() {
       round1.firstRoll(8);
       expect(round1.score()).toEqual(8);
     });
   });
 
-  // describe("secondRoll", function() {
-  //   it("if a spare, it should add roll to roundscore", function() {
-  //     round1.firstRoll('spare');
-  //     expect(round1.score()).toEqual(10);
-  //   });
-  //
-  //   it("if not a spare, it should add roll to roundscore", function() {
-  //     round1.firstRoll(8);
-  //     expect(round1.score()).toEqual(8);
-  //   });
-  //
-  //   it("if a total of 10 points is entered, it should record spare", function() {
-  //     round1.firstRoll('strike');
-  //     expect(round1.score()).toEqual(10);
-  //   });
-  // });
+  describe("secondRoll", function() {
+    it("if a spare, it should add rollPoints to roundScore & record spare", function() {
+      round1.firstRoll(2);
+      round1.secondRoll('spare');
+      expect(round1.score()).toEqual(10);
+      expect(round1.spare).toEqual(true);
+    });
+
+    it("if not a spare, it should add rollPoints to roundScore", function() {
+      round1.firstRoll(1);
+      round1.secondRoll(8);
+      expect(round1.score()).toEqual(9);
+      expect(round1.spare).toEqual(false);
+    });
+
+    it("if more than 10 points is entered, it should record spare & set roundScore to 10", function() {
+      round1.firstRoll(3);
+      round1.secondRoll(8);
+      expect(round1.score()).toEqual(10);
+      expect(round1.spare).toEqual(true);
+    });
+  });
 
 });
--- src/Round.js
+++ src/Round.js
@@ -1,20 +1,36 @@
 function Round() {
   this._score = 0;
-  this._complete = false;
-  this._strike = false;
-  this._spare = false;
+  this.complete = false;
+  this.strike = false;
+  this.spare = false;
 };
 
 Round.prototype.score = function() {
   return this._score;
 };
 
-Round.prototype.firstRoll = function(roll) {
-  if (roll === 'strike') {
-    this._score += 10;
-    this._strike = true;
+Round.prototype.firstRoll = function(rollPoints) {
+  if (rollPoints === 'strike') {
+    this._score = 10;
+    this.strike = true;
   } else {
-    this._score += roll;
-    this._strike = false;
+    this._score = rollPoints;
+    this.strike = false;
+  }
+}
+
+Round.prototype.secondRoll = function(rollPoints) {
+  if (rollPoints === 'spare') {
+    this._score = 10;
+    this.spare = true;
+    this.complete = false;
+  } else if ((rollPoints + this._score) >= 10) {
+    this._score = 10;
+    this.spare = true;
+    this.complete = false;
+  } else {
+    this._score += rollPoints;
+    this.spare = false;
+    this.complete = true;
   }
 }
